aeb8398d1966c6873ee00ca7d2337563a756f5ce
skyDe <sky_de1991@outlook.com>
2024-03-20 07:41:35 -0800

    Binary search added

---
+++ binarySearch_python.py
@@ -0,0 +1,40 @@
+
+# # Binary with While loop
+
+# myList = [1,2,3,4,5,6,7,8,9,10]
+# target = 7
+
+# def my_binary_search_while(list,target):
+#   low = 0
+#   high = len(list) - 1
+#   mid = 0
+#   while low <= high:
+#       mid = (high + low) // 2
+#       if list[mid] < target:
+#         low = mid -1   
+#       elif list[mid] > target:
+#         high = mid + 1   
+#       if list[mid] == target:
+#        return mid
+          
+#   return None    
+    
+# print(my_binary_search_while(myList,target))
+
+
+# # Binary with recursion
+myList = [1,2,3,4,5,6,7,8,9,10]
+
+def my_binary_search_recursive(low,high,list,target):
+  if(high >= low):
+    mid = (high + low) // 2
+    if list[mid] == target:
+      return mid
+    elif list[mid] > target:
+      return my_binary_search_recursive(low, mid - 1 , list, target)
+    else:
+      return my_binary_search_recursive(mid + 1, high , list, target)
+  return None
+
+
+print(my_binary_search_recursive(0,len(myList) - 1, myList,2))
--- python_starter.py
+++ python_starter.py
@@ -1,5 +1,11 @@
+# --------------------------------------------------------
 print("First python log")
 
 name = input("Insert your name: ")
 age = int(input("Insert your age: "))
 print(name + " is " + str(age) + " years old")
+
+# --------------------------------------------------------
+the_world_is_flat = True
+if the_world_is_flat:
+    print("Be careful not to fall off!")
