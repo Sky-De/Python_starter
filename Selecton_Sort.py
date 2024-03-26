46a582028d296329066c55eeb55a22f7f7203d5d
skyDe <sky_de1991@outlook.com>
2024-03-26 14:41:37 -0800

    selecton sort added

---
+++ Selecton_Sort.py
@@ -0,0 +1,19 @@
+
+# # Binary with While loop
+
+myList = [5,6,33,2,3,1,9,11,10,4,7,8]
+
+
+def selection_sort(arr):
+  for i in range(0,len(arr)):
+    indexOfMin = i
+    for j in range(i+1,len(arr)):
+      if arr[indexOfMin] > arr[j]:
+        indexOfMin = j
+      if(indexOfMin != i):
+        arr[i],arr[indexOfMin] = arr[indexOfMin], arr[i]
+  return arr
+
+sortedList = selection_sort(myList)
+
+print(sortedList)
