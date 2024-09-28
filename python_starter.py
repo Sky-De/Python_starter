057f7cfe17598e6333eea5038d662e8d583d4d02
skyDe <sky_de1991@outlook.com>
2024-09-28 08:20:50 -0800

    list and some of list method added / next lesson is tuples

--- python_starter.py
+++ python_starter.py
@@ -1,11 +1,54 @@
-# --------------------------------------------------------
-print("First python log")
+# # --------------------------------------------------------
+# print("First python log")
 
-name = input("Insert your name: ")
-age = int(input("Insert your age: "))
-print(name + " is " + str(age) + " years old")
+# name = input("Insert your name: ")
+# age = int(input("Insert your age: "))
+# print(name + " is " + str(age) + " years old")
 
-# --------------------------------------------------------
-the_world_is_flat = True
-if the_world_is_flat:
-    print("Be careful not to fall off!")
+# # --------------------------------------------------------
+# the_world_is_flat = True
+# if the_world_is_flat:
+#     print("Be careful not to fall off!")
+
+# # Lists--------------------------------------------------------
+# friends = ["Kevin","Max","Sam","Mike","Sky"]
+
+# print(friends[0]) # first element
+# print(friends[-1]) # last element
+# print(friends[1:]) # index 1 and rest
+# print(friends[2:4]) # from index 2 to index 3
+
+# friends[0] = "newF"
+# print(friends)
+
+# # Functions used on list----------------------------------------
+# listOne = [1,2,3,4,5,9,8,7,6]
+# listTwo = ["a","s","z","b","c","d","d","d"]
+
+# listTwo.extend(listOne) # adds two list
+
+# listTwo.append("newItem") # adds new item at the end of list
+
+# listTwo.insert(1,"insertedItem") # adds new item at specified index
+# # listTwo.remove(listTwo[1])
+# listTwo.remove("insertedItem") # removes item from list
+
+# # listTwo.clear() # removes all item from list
+# print(listTwo.pop()) # pop an item from end of list
+
+
+# print(listTwo.index("a")) # checks item in list
+# print(listTwo.count("d")) # counts items in list
+
+# listOne.sort() # sorts lists
+# print(listOne) 
+# listOne.reverse()
+# print(listOne)
+# print(listTwo)
+
+# newlistCopy = listOne.copy()
+# print(newlistCopy)
+
+
+
+# # Tuples--------------------------------------------------------
