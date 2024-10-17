bd26d18d54fdc43fe9ceb8c58ccde06597ec15b5
Mohammad <Mohammad.Aghajanpour@antriconsulting.com>
2024-10-17 20:53:26 +0330

    bs practice done for today

--- z_Repeat_Is_The_Key.py
+++ z_Repeat_Is_The_Key.py
@@ -96,20 +96,40 @@ target_number = 9
 # print(binaryS(myList,target_number))
 
 
-def bs(list,target):
+# def bs(list,target):
+#     low = 0
+#     mid = 0
+#     high = len(list) - 1
+    
+#     while low <= high:
+#         mid = (low + high) // 2
+#         mid_item = list[mid]
+#         if mid_item == target:
+#             return mid
+#         elif mid_item > target:
+#             high = mid - 1
+#         elif mid_item < target:
+#             low = mid + 1
+#     return None
+
+# print(bs(myList,target_number))
+
+
+
+def myBs(arr,target):
     low = 0
     mid = 0
-    high = len(list) - 1
-    
+    high = len(arr) -1
     while low <= high:
-        mid = (low + high) // 2
-        mid_item = list[mid]
-        if mid_item == target:
+        mid = (high + low ) // 2
+        midItem = arr[mid]
+        if midItem == target:
             return mid
-        elif mid_item > target:
-            high = mid - 1
-        elif mid_item < target:
+        elif midItem < target:
             low = mid + 1
+        elif midItem > target:
+            high = mid -1
     return None
 
-print(bs(myList,target_number))
+print(myBs(myList,target_number))
+        
